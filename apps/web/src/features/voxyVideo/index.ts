92dcd9cbaed53240e3886da8e0def5639a4f5af3
VOGADMINRGF <rgf@voiceopengov.de>
2026-09-21 19:38:36 +0200

    fix(voxy): keep local composition runtime out of client-safe barrel

--- apps/web/src/features/voxyVideo/index.ts
+++ apps/web/src/features/voxyVideo/index.ts
@@ -2,7 +2,5 @@ export * from "./contracts";
 export * from "./dualVoiceArchitecture";
 export * from "./dualVoiceExplainerPilot";
 export * from "./homepageReferenceFilms";
-export * from "./localCompositionRuntime";
-export * from "./localCompositionRuntimeService";
 export * from "./localTts";
 export * from "./voicedExplainerV1";
