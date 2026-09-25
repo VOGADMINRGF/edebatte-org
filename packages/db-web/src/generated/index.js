44c21714907b340b04fb75601adf2f9b165e78bb
VOGADMINRGF <rgf@voiceopengov.de>
2026-09-25 16:58:05 +0000

    chore(newsletter): drop generated scope leak

--- packages/db-web/src/generated/index.js
+++ packages/db-web/src/generated/index.js
@@ -291,7 +291,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/home/runner/work/edebatte-org/edebatte-org/packages/db-web/src/generated",
+      "value": "/Users/RF/Arbeitsmappe/edebatte-org/packages/db-web/src/generated",
       "fromEnvVar": null
     },
     "config": {
@@ -300,12 +300,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "darwin-arm64",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/home/runner/work/edebatte-org/edebatte-org/prisma/web/schema.prisma",
+    "sourceFilePath": "/Users/RF/Arbeitsmappe/edebatte-org/prisma/web/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -319,7 +319,6 @@ const config = {
   ],
   "activeProvider": "postgresql",
   "postinstall": false,
-  "ciName": "GitHub Actions",
   "inlineDatasources": {
     "db": {
       "url": {
@@ -368,8 +367,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "packages/db-web/src/generated/libquery_engine-debian-openssl-3.0.x.so.node")
+path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
+path.join(process.cwd(), "packages/db-web/src/generated/libquery_engine-darwin-arm64.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "packages/db-web/src/generated/schema.prisma")
--- packages/db-web/src/generated/wasm.js
+++ packages/db-web/src/generated/wasm.js
@@ -290,7 +290,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/home/runner/work/edebatte-org/edebatte-org/packages/db-web/src/generated",
+      "value": "/Users/RF/Arbeitsmappe/edebatte-org/packages/db-web/src/generated",
       "fromEnvVar": null
     },
     "config": {
@@ -299,12 +299,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "darwin-arm64",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/home/runner/work/edebatte-org/edebatte-org/prisma/web/schema.prisma",
+    "sourceFilePath": "/Users/RF/Arbeitsmappe/edebatte-org/prisma/web/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -318,7 +318,6 @@ const config = {
   ],
   "activeProvider": "postgresql",
   "postinstall": false,
-  "ciName": "GitHub Actions",
   "inlineDatasources": {
     "db": {
       "url": {
